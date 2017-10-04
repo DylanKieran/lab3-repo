058ad86650469b154a0e031e4c6b593e2c4253f2
Superdizzy17 <dylankieran@hotmail.com>
2017-10-04 15:47:18 +0000

    App.py updated

--- app.py
+++ app.py
@@ -19,6 +19,25 @@ def hello(): # Name of the method
     cur.execute('''SELECT * FROM students''') # execute an SQL statment
     rv = cur.fetchall() #Retreive all rows returend by the SQL statment
     return str(rv)      #Return the data in a string format
+
+@app.route("/add")
+def add():
+	cur = mysql.connection.cursor()
+	cur.execute('''Insert into students (studentName, email) values ("Dylan Kieran","dylankieran@hotmail.com"); commit; ''')
+	return 'Added new student x'
+
+@app.route("/update")
+def update():
+	cur = mysql.connection.cursor()
+	cur.execute('''Update students set studentName = "Bob Dylan" WHERE studentName like "Dylan Kieran"; commit;''')
+	return 'Updated student x'
+
+@app.route("/delete")
+def delete():
+        cur = mysql.connection.cursor()
+        cur.execute('''DELETE from students where studentName like "Bob Dylan"; commit;''')
+        return 'Deleted student x'  
+
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port='5000') #Run the flask app at port 5000
 
